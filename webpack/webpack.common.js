6a55132062778a78afbd02f962a6f150ae2bff0e
Niklas Jurij Plessing <contact@niklasplessing.net>
2021-05-07 12:26:39 +0200

    A

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -43,7 +43,7 @@ module.exports = {
           options: {
             name: '[name].[ext]',
             outputPath: 'images',
-            publicPath: '../',
+            publicPath: 'images',
           },
         },
       },
